2a6e059f6134aa25e6af29289967c4cc2841513f
arslandurrani <arslandurrani@outlook.com>
2017-11-23 17:07:29 +0500

    changes

--- CustomeEvent/src/main/java/com/amazonaws/lambda/demo/LambdaFunction.java
+++ CustomeEvent/src/main/java/com/amazonaws/lambda/demo/LambdaFunction.java
@@ -2,6 +2,7 @@ package com.amazonaws.lambda.demo;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -11,21 +12,33 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class LambdaFunction implements RequestHandler<Object, String> {
 
     @Override
-    public String handleRequest(Object input, Context context) {    	
-    	  	
+    public String handleRequest(Object input, Context context) {    	    	
+    	
     	JSONObject jsonObj = null;
     			
 		try {
 			
-			jsonObj = new JSONObject(new ObjectMapper().writeValueAsString(input));
-			context.getLogger().log("jsonObj:" + jsonObj);		
+			jsonObj = new JSONObject(new ObjectMapper().writeValueAsString(input));  // convert object to json object			
+			context.getLogger().log("jsonObj before:" + jsonObj);				
 			
 		} catch (JsonProcessingException | JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}		
+		}				
 		
+		
+		try {
+			jsonObj.getJSONObject("response").put("autoVerifyEmail", true);
+			jsonObj.getJSONObject("response").put("autoConfirmUser", true);
+			
+			context.getLogger().log("jsonObj after:" + jsonObj);					
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
         // TODO: implement your handler
-        return "Hello from Lambda!";
+        return jsonObj.toString();
     }
 }
