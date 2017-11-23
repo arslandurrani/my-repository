f1b0e0800c84e9e71e49996fca84a407d665ae63
arslandurrani <arslandurrani@outlook.com>
2017-11-23 11:52:10 +0500

    putting lambda project into git

---
+++ CustomeEvent/src/main/java/com/amazonaws/lambda/demo/LambdaFunction.java
@@ -0,0 +1,34 @@
+package com.amazonaws.lambda.demo;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class LambdaFunction implements RequestHandler<Object, String> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {    	
+    	  	
+    	JSONObject jsonObj = null;
+    	
+		try {
+			
+			jsonObj = new JSONObject(new ObjectMapper().writeValueAsString(input));
+			context.getLogger().log("jsonObj:" + jsonObj);			
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}      
+    	
+        // TODO: implement your handler
+        return "Hello from Lambda!";
+    }
+}
